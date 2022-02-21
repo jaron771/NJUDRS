400cf5a9579b8a7bb6a9722d2b0df759fd534085
陈东骁 <mf21320014@smail.nju.edu.cn>
2022-02-21 19:39:01 +0800

    finish IEEEConfig.java

---
+++ svc-prediction/src/main/java/com/nju/svcprediction/datasource/IEEEConfig.java
@@ -0,0 +1,47 @@
+package com.nju.svcprediction.datasource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = {"com.nju.svcprediction.dataMapper.ieee"}, sqlSessionTemplateRef = "sqlSessionTemplate")
+public class IEEEConfig {
+
+    @Bean(name = "ieeedb")
+    // 设置数据库属性来源
+    @ConfigurationProperties(prefix = "spring.datasource.ieee")
+    public DataSource dataSource(){
+        // 默认是Hikari
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "ieeeSqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("ieeedb")DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        // 设置Mapper.xml文件的扫描处
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/IEEE*.xml"));
+        bean.setDataSource(dataSource);
+        return bean.getObject();
+    }
+
+    @Bean(name = "ieeeTransactionManager")
+    public DataSourceTransactionManager transactionManager(@Qualifier("ieeedb") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("ieeeSqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
