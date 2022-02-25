c41a8307e910461bea5e7dc6894a5d608f456057
陈东骁 <mf21320014@smail.nju.edu.cn>
2022-02-25 21:26:40 +0800

    finish EdgeContent

---
+++ svc-prediction/src/main/java/com/nju/svcprediction/domain/EdgeContent.java
@@ -0,0 +1,20 @@
+package com.nju.svcprediction.domain;
+
+public class EdgeContent{
+    private int commonWorks;
+
+    public EdgeContent() {
+    }
+
+    public EdgeContent(int commonWorks) {
+        this.commonWorks = commonWorks;
+    }
+
+    public int getCommonWorks() {
+        return commonWorks;
+    }
+
+    public void setCommonWorks(int commonWorks) {
+        this.commonWorks = commonWorks;
+    }
+}
