661029c44083e02604ace7ebcd06ea82bab3cd8f
陈东骁 <mf21320014@smail.nju.edu.cn>
2022-03-06 20:21:03 +0800

    update App starter

---
+++ svc-prediction/src/main/java/com/nju/svcprediction/SvcPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.nju.svcprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcPredictionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvcPredictionApplication.class, args);
+    }
+
+}
