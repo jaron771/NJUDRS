b5f30292c3a5c6403251c6d47f62e79d63946235
jaron771 <35825744+jaron771@users.noreply.github.com>
2022-04-17 20:09:51 +0800

    Finish SvcRecommendTest

---
+++ svc-recommend/src/test/java/com/nju/svcrecommend/SvcRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nju.svcrecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcRecommendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
